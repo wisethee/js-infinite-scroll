d1bf797ce86d473407647038505307f47ab91842
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2022-06-04 21:10:29 +0100

    Add HTML elements, styles and JS functionality

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,1 +1,72 @@
-export class App {}
+export class App {
+  constructor() {
+    this.sss();
+  }
+
+  imageCount = 5;
+  apiKey = 'QGSODD3PTsRcvsqgY69oajG-BWagMdhvIBIaCjrkBVM';
+  apiURL = `https://api.unsplash.com/photos/random/?client_id=${this.apiKey}&count=${this.imageCount}`;
+
+  imageContainer = document.getElementById('image-container');
+  loader = document.getElementById('loader');
+
+  photosArray: any[] = [];
+  ready: boolean = false;
+  imagesLoaded = 0;
+  totalImages: number = 0;
+
+  async getPhotos() {
+    try {
+      const response = await fetch(this.apiURL);
+      this.photosArray = await response.json();
+      this.displayPhotos();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  displayPhotos() {
+    this.imagesLoaded = 0;
+    this.photosArray.forEach((photo) => {
+      this.totalImages = this.photosArray.length;
+      const item = document.createElement('a');
+      this.setAttributes(item, { href: photo.links.html, target: '_blank' });
+      const img = document.createElement('img');
+      this.setAttributes(img, {
+        src: photo.urls.regular,
+        alt: photo.alt_description,
+        tittle: photo.alt_description
+      });
+      img.addEventListener('load', this.imageLoaded.bind(this));
+      item.appendChild(img);
+      this.imageContainer.appendChild(item);
+    });
+  }
+
+  imageLoaded() {
+    this.imagesLoaded++;
+    if (this.imagesLoaded === this.totalImages) {
+      this.ready = true;
+      this.loader.hidden = true;
+    }
+  }
+
+  setAttributes(element: any, attributes: any) {
+    for (const key in attributes) {
+      element.setAttribute(key, attributes[key]);
+    }
+  }
+
+  sss() {
+    window.addEventListener('scroll', () => {
+      if (
+        window.innerHeight + window.scrollY >=
+          document.body.offsetHeight - 1000 &&
+        this.ready === true
+      ) {
+        this.ready = false;
+        this.getPhotos();
+      }
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,5 @@ import './styles/main.scss';
 import { App } from './app/app';
 
 const app = new App();
+
+app.getPhotos();
